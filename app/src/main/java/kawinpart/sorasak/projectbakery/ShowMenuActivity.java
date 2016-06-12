72612a70e1b31ae04d71cab14f03e933158a742c
sorasakUoU <nightontherock@gmail.com>
2016-06-13 01:31:03 +0700

    Design UI ShowMenuActivity Finish

---
+++ app/src/main/java/kawinpart/sorasak/projectbakery/ShowMenuActivity.java
@@ -0,0 +1,13 @@
+package kawinpart.sorasak.projectbakery;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ShowMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_menu);
+    }
+}
